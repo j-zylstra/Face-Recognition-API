ca64b884c551bc64eb26c5bbd7fd7b67c26756e7
Joe Zylstra <j-zylstra@hotmail.com>
2023-07-27 14:07:31 +0100

    updating database

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
+      host : 'postgresql-elliptical-45204',
       user : 'postgres',
       password : 'password123',
       database : 'smart-brain'
