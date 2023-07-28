ebc44dc604f9006e34436fa03d7136818b693451
Joe Zylstra <j-zylstra@hotmail.com>
2023-07-28 10:47:41 +0100

    cors fix

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const app = express();
 
 
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors({origin: "https://localhost:8000"}));
 
 
 app.get('/', (req, res) => { res.send('it is working') })
