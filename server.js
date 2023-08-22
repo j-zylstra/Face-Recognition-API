d59b7fe7b9adb80db09d86d2212745aaf631994a
Joe Zylstra <j-zylstra@hotmail.com>
2023-08-22 15:18:54 +0100

    db change

--- server.js
+++ server.js
@@ -14,14 +14,21 @@ const db = knex({
   client: 'pg',
   connection: {
     connectionString: process.env.DATABASE_URL,
-    ssl: true,
-    }
+    ssl: {
+      rejectUnauthorized: false,
+    },
+    host: process.env.DATABASE_HOST,
+    port: 5432,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PW,
+    database: process.env.DATABASE_DB,
+  },
 });
 
 const app = express();
 
 app.use(bodyParser.json());
-app.use(cors({origin: "https://vast-caverns-20756-f8729b26975b.herokuapp.com"}));
+app.use(cors({origin: "https://vast-caverns-20756-f8729b26975b.herokuapp.com", }));
 
 
 app.get('/', (req, res) => { res.send('it is working') })
