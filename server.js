eb6c1c515caf7b388eee5e549571dd366a7f5bcc
Joe Zylstra <j-zylstra@hotmail.com>
2023-08-25 12:46:07 +0100

    cors update

--- server.js
+++ server.js
@@ -35,6 +35,7 @@ app.use(function (req, res, next){
       "Access-Control-Allow-Methods",
       "GET,HEAD,OPTIONS,POST,PUT,DELETE"
   );
+  res.header("Access-Control-Allow-Credentials", true);
   res.header(
       "Access-Control-Allow-Headers",
       "Origin, X-Requested-With, Content-Type, Accept, Authorization"
