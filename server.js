0f7caaaaa809514109c47eedd44b0a9508b1099b
Joe Zylstra <j-zylstra@hotmail.com>
2023-08-25 12:58:13 +0100

    "allow-origin"

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 app.use(function (req, res, next){
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "https://face-finder-1b614a02d3d9.herokuapp.com/");
   res.header(
       "Access-Control-Allow-Methods",
       "GET,HEAD,OPTIONS,POST,PUT,DELETE"
